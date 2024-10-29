893e1875c9000da95fc6af8a50295f57a612bd81
Ubuntu <ubuntu@ip-172-31-21-122.ec2.internal>
2024-10-29 05:20:51 +0000

    added IP_blocking using postgreSQL

--- project/TargetMachine/backend/database.js
+++ project/TargetMachine/backend/database.js
@@ -9,6 +9,11 @@ const pool = new Pool({
     port: process.env.DB_PORT,
 })
 
+console.log(process.env.DB_HOST);
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASSWORD);
+console.log(process.env.DB_DATABASE);
+
 pool.connect()
   .then(() => console.log('Connected to the database'))
   .catch(err => console.error('Connection error', err.stack));
--- project/TargetMachine/backend/server.js
+++ project/TargetMachine/backend/server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 
 const app = express();
 const port = 5050;
-
+const IPCounts = {}
 
 // Serve static files from the React app (the client build folder)
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
@@ -13,9 +13,51 @@ app.use(express.static(path.join(__dirname, '../frontend/dist')));
 // Middleware to parse JSON request bodies
 app.use(express.json());
 
+
+const handleNewIP = async (ip, res) => {
+    if (IPCounts[ip]) {
+        if (IPCounts[ip] > 25) {
+	    try {
+            	await db.query(
+                    'INSERT INTO blocked_ips (ip_address) VALUES ($1) ON CONFLICT (ip_address) DO NOTHING',
+                    [ip]
+            	);
+            	console.log(`IP Address ${ip} has been added to the blocked_ips table.`);
+            } catch (error) {
+            	console.error('Error adding IP to blocked_ips:', error);
+            	return res.status(500).json({ message: 'Internal Server Error' });
+            }	
+	} else {
+	    IPCounts[ip] += 1;
+	}
+    } else {
+        IPCounts[ip] = 1;
+    }
+}
+
+
 // Handle login POST request
-app.post('/login', (req, res) => {
+app.post('/login', async (req, res) => {
   const { username, password } = req.body;
+  const requestIP = req.ip;
+
+  try {
+    // Query to check if IP is in blocked_ips
+    const { rowCount } = await db.query(
+      'SELECT 1 FROM blocked_ips WHERE ip_address = $1',
+      [requestIP]
+    );
+
+    // If IP is found in blocked_ips, return an error
+    if (rowCount > 0) {
+      return res.status(403).json({ message: 'Access Denied' });
+    } 
+  } catch (error) {
+    console.error('Database query error: ', error);
+    res.status(500).json({message: 'Internal Server Error'});
+  }
+
+  handleNewIP(requestIP, res);
 
   // Simulated login check
   if (username === 'admin' && password === 'password') {
@@ -23,7 +65,7 @@ app.post('/login', (req, res) => {
   } else {
     res.status(401).json({ message: 'Invalid Credentials' });
   }
-  console.log(res.status);
+  console.log(res.statusCode)
 });
 
 // Catch-all route to serve the React frontend
