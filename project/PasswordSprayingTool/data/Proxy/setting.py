3118b10d7ef32c570fce61d8ed70e297b960f3d3
Sotatek-DuyNguyen <duynp.hn@gmail.com>
2024-11-04 00:32:30 -0600

    Delete unneccessary file

--- project/PasswordSprayingTool/data/Proxy/setting.py
+++
@@ -1,8 +0,0 @@
-ROTATING_PROXY_LIST_PATH = '/my/path/proxies.txt' # Path that this library uses to store list of proxies
-NUMBER_OF_PROXIES_TO_FETCH = 5 # Controls how many proxies to use
-
-
-DOWNLOADER_MIDDLEWARES = {
-    'rotating_free_proxies.middlewares.RotatingProxyMiddleware': 610,
-    'rotating_free_proxies.middlewares.BanDetectionMiddleware': 620,
-}
